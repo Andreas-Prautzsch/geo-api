f1117aee8fe06fd866cfdacbb22952c05972b0bf
Andreas Prautzsch <info@andreas-prautzsch.de>
2025-10-14 03:44:31 +0200

    feat(docker): update Photon service configuration to use JAR URL and enhance entrypoint script

--- docker/photon/entrypoint.sh
+++ docker/photon/entrypoint.sh
@@ -10,6 +10,8 @@ PBF_URL="${PHOTON_PBF_URL:-https://download.geofabrik.de/europe/germany-latest.o
 PBF_FILE="${PHOTON_PBF_FILE:-germany-latest.osm.pbf}"
 IMPORT_FORCE="${PHOTON_FORCE_REIMPORT:-false}"
 
+cd /opt/photon
+
 mkdir -p "${DATA_DIR}"
 
 PBF_PATH="${DATA_DIR}/${PBF_FILE}"
